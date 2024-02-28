ad861a2ad3b2ec9591b00fc3011e3c27464bec63
Ekatya6 <ekaterinasoloveyeva@gmail.com>
2024-02-28 17:49:03 +0500

    add lesson2:task4

---
+++ lesson2/task4/Program.cs
@@ -0,0 +1,39 @@
+﻿// Дано 5 гирь с разным весом в случайном порядке. Определить вес самой тяжелой гири. 
+
+using System.Globalization;
+
+int n = 5;
+int[] array = {2,4,7,3,9}
+;
+int i = 0;
+int max = array[0];
+
+while(i<n)
+{
+    if (array[i] > max)
+    {
+        max = array[i];
+    }
+    i = i + 1;
+}
+System.Console.WriteLine(max);
+
+max = array[0];
+for(int j = 0; j < n; j++)  //  j++ - это новый оператор Инкремент, работает как счетчик +1
+{
+    if (array[j] > max)
+    {
+        max = array[j];
+    }
+}
+System.Console.WriteLine(max);
+
+max = array[0];
+foreach (int e in array)
+{
+    if (e > max)
+    {
+        max = array[e];
+    }
+}
+System.Console.WriteLine(max);
