010eee6ac2a4fd31e2effe3438a825aa7dc4f964
Ekatya6 <ekaterinasoloveyeva@gmail.com>
2024-03-05 17:21:36 +0500

    add lesson3:task1

---
+++ lesson3/task1/Program.cs
@@ -0,0 +1,11 @@
+﻿// Вычислить значение формулы a * b : c + d, где a,b,c,d  - некторые целые числа. Решение оформить в виде функции. 
+ double CalkulateFormula(int a, int b, int c, int d)
+ {
+    double numenator = a * b;                  // числитель
+    int denomenator = c + d;                   // знаменатель
+    double result = numenator / denomenator;
+    return result;
+ }
+
+ double result = CalkulateFormula(1,2,3,4);    // когда мы Запускаем функцию, то используем термин Аргументы, если Создаем - то Параметры
+System.Console.WriteLine(result);
