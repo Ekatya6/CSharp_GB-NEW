45758e3f7319578e78a34a7d38afbf360cad94f8
Ekatya6 <ekaterinasoloveyeva@gmail.com>
2024-03-11 14:56:19 +0500

    add lesson4:task1

---
+++ lesson4/task1/Program.cs
@@ -0,0 +1,30 @@
+﻿// Создать двумерный массив с размерами 3 х 5, состоящий из целых чисел. Вывести его элементы на экран.
+
+using System;
+class Program
+{
+    static void Main()
+    {
+        int[,] array = new int[3, 5];
+        Random rnd = new Random();
+
+        for (int i = 0; i < 3; i++)      // если мы указываем нулевое измерение, то мы запрашиваем количество строк, i - строка
+        {
+            for (int j = 0; j < 5; j++)  // (1) - запрашиваем количество столбцов в этом массиве, j - столбец
+
+            {
+                array[i, j] = rnd.Next(1, 11);
+            }
+        }
+
+        for (int i = 0; i < 3; i++)
+        {
+            for (int j = 0; j < 5; j++)
+            {
+                System.Console.Write(array[i, j] + " ");
+            }
+            System.Console.WriteLine();
+        }
+    }
+
+}
