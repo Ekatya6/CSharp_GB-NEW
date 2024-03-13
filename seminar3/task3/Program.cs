6bd1fe47ec43f6e6aed33f7c89661beffade1fcb
Ekatya6 <ekaterinasoloveyeva@gmail.com>
2024-03-13 17:05:19 +0500

    add seminar3:task3

---
+++ seminar3/task3/Program.cs
@@ -0,0 +1,19 @@
+﻿// Считато строку с консоли, состоящую из латинских букв в нижнем регистре. Выяснить, сколько исреди введенных букв гласных.  
+// Например: 
+// "hello" => 2
+// "world" => 1 
+
+System.Console.WriteLine("введите строчку");
+string userInput = Console.ReadLine();
+
+string vowels = "aeiud";   // vowels - гласные
+int count = 0;
+
+foreach (var e in userInput)
+{
+    if (vowels.Contains(e))    // функция Contains говорит нам, есть ли в этой строчке какой-то символ
+    {
+        count++;
+    }
+}
+System.Console.WriteLine(count);
