4d7a52608347b39aa70e790b658cdac849cbeafd
Ekatya6 <ekaterinasoloveyeva@gmail.com>
2024-03-13 15:25:27 +0500

    add seminar3:task1

---
+++ seminar3/task1/Program.cs
@@ -0,0 +1,12 @@
+﻿// Задайте массив символов (тип char []). Создайте строку из стмволов этого массива. 
+
+char[] array = new char[]{'t', '4', '#', 'y', '9'};
+
+string str ="";
+
+for (int i = 0; i < array.Length; i++)
+{
+    str = str + array[i]; // чтобы преобразовать тип char в string надо или к str +, или char конвертировать в string
+}
+
+System.Console.WriteLine(str);
