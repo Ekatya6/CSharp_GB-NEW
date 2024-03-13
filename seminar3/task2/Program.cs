e5e14ef56f231ea946f978d1be60a6a89d3ff199
Ekatya6 <ekaterinasoloveyeva@gmail.com>
2024-03-13 16:39:38 +0500

    add seminar3:task2

---
+++ seminar3/task2/Program.cs
@@ -0,0 +1,10 @@
+﻿// На основе символов сторки (тип string) сформировать массив символов (тип char[]). Вывести массив на экран. 
+
+string str = Console.ReadLine();
+char[] array = new char[str.Length];
+
+for (int i = 0; i< array.Length; i++)
+{
+    array[i] = str[i];
+    System.Console.WriteLine(array[i]);
+}
