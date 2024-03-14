dd3c6004cade3334fef949e6a4c9040d0f004024
Ekatya6 <ekaterinasoloveyeva@gmail.com>
2024-03-14 18:05:36 +0500

    add seminar:task3

---
+++ seminar4/task3/Program.cs
@@ -0,0 +1,29 @@
+﻿// Считать строку с консоли, содержащую латинские буквы. Вывести на экран согласные буквы этой строки. 
+// Использовать рекурсию, не использовать цикл. Например: 
+// "hello" => h l l,  "world" =. r l d 
+
+//---------------ОБЪЯВЛЕНИЕ ФУНКЦИЙ----------------
+
+bool IsConsonant(char c)  // логический тип данных, "согласные"
+{
+    return "bcdfghjklmnpqrstvwxz".Contains(c); // метод Contains - если заданный символ в строке. c = 'l'
+}
+
+void PrintWithoutVowels(string str, int index = 0)  
+{
+    if (index >= str.Length) return;
+
+    char c = char.ToLower(str[index]);
+
+    if(IsConsonant(c))
+    {
+        System.Console.Write(str[index] + " ");
+    }
+    PrintWithoutVowels(str, index + 1);
+}
+
+//---------------ОСНОВНОЙ КОД----------------------
+
+System.Console.WriteLine("введите строку");
+string inputStr = Console.ReadLine();
+PrintWithoutVowels(inputStr);   // здесь к аргументу можно добавить цифру строчки, откуда начать 
